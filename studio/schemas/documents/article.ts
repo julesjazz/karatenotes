4b5f3670ccaf08c4e55ddb0ed639d9ceba993ef6
Julia⭐️ <julesrsilver@gmail.com>
2022-06-17 14:38:50 -0600

    article tweak

--- studio/schemas/documents/article.ts
+++ studio/schemas/documents/article.ts
@@ -2,22 +2,51 @@ export default {
   name: 'article',
   title: 'Article',
   type: 'document',
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+      shared: 'shared',
+      category: 'category.title',
+    },
+    prepare({title, category, shared, media}: any) {
+      return {
+        title: title,
+        subtitle: `${shared ? '🟢' : '🔴'} ${category ? category : 'No category'}`,
+        media: media || '',
+      }
+    },
+  },
+  groups: [
+    {name: 'details', title: 'Details', default: true},
+    {name: 'body', title: 'Body'},
+  ],
   fields: [
     {
+      group: 'details',
       name: 'title',
       title: 'Title',
       type: 'string',
     },
     {
-      name: 'published',
-      title: 'Published',
+      group: 'details',
+      name: 'shared',
+      title: 'Shared',
       type: 'boolean',
       initialValue: false,
-      options: {
-        layout: 'checkbox',
-      },
+      // options: {
+      //   layout: 'checkbox',
+      // },
     },
     {
+      group: 'details',
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: {type: 'category'},
+    },
+    {
+      group: 'details',
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
@@ -25,21 +54,16 @@ export default {
         hotspot: true,
         storeOriginalFilename: false,
         metadata: [
-          'blurhash',   // Default: included
-          'lqip',       // Default: included
-          'palette',    // Default: included
+          'blurhash', // Default: included
+          'lqip', // Default: included
+          'palette', // Default: included
           // 'exif',       // Default: not included
           // 'location',   // Default: not included
         ],
       },
     },
     {
-      name: 'category',
-      title: 'Category',
-      type: 'reference',
-      to: {type: 'category'},
-    },
-    {
+      group: 'body',
       name: 'body',
       title: 'Body',
       type: 'blockContent',
